fc81c3a23f531e3d94d12f09059cc48ab5dffc31
ryanh614 <ryanthausner@gmail.com>
2026-03-28 16:22:24 -0700

    first coding project - expense tracker

---
+++ fundamentals/expense_tracker.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+//personal budget tracker using vectors and structs 
+//add income or expense with category and amount 
+//store all transations in a vector 
+//show total income, expenses, and balance
+//show spending breakdown by category 
+//keep running until user quits 
+
+
+
+
+//define struct for transaction 
+struct Transaction {
+    string type;
+    string category;
+    float amount; 
+}; 
+
+
+int main() {
+
+    return 0; 
+}
