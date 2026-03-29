b975729e3834e69ca3f3204723fc25f07960ad6c
ryanh614 <ryanthausner@gmail.com>
2026-03-29 15:55:22 -0700

    write functions for each task, setup switch and loop to exit

--- fundamentals/expense_tracker.cpp
+++ fundamentals/expense_tracker.cpp
@@ -25,9 +25,45 @@ float total_income = 0;
 float total_expenses = 0;
 float balance = 0;
 
+void add_income() {
+    cin >> total_income; 
+}
 
+void add_expense() {
+    cin >> total_expenses; 
+}
+
+void show_balance() {
+    cout << "Balance: " << balance << endl;
+}
+
+void show_spending_breakdown() {
+    cout << "Spending Breakdown: " << endl;
+    for (Transaction t : transactions) {
+        cout << t.category << ": " << t.amount << endl;
+    }
+}
 
 int main() {
+    //start with the structure 
+    int input; 
+    cout << "Menu: \n 1. Add Income \n 2. Add Expense \n 3. Show Balance \n 4. Show Spending Breakdown \n 5. Quit \n";
+    cin >> input; 
+    while (input != 5) {
+        switch (input) {
+            case 1: 
+                add_income();
+                break;
+            case 2: 
+                add_expense();
+                break; 
+            case 3: 
+                show_balance();
+                break; 
+            case 4: 
+                show_spending_breakdown();
+        }
+    }
 
     return 0; 
 }
