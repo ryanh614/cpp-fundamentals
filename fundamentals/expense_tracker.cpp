33f4ad65f86cc7ca003e0438fb43e0586edd5eac
ryanh614 <ryanthausner@gmail.com>
2026-03-29 16:17:06 -0700

    finish add_expense and add_income functions

--- fundamentals/expense_tracker.cpp
+++ fundamentals/expense_tracker.cpp
@@ -20,48 +20,78 @@ struct Transaction {
     float amount; 
 }; 
 
-vector<Transaction> transactions; 
-float total_income = 0;
-float total_expenses = 0;
-float balance = 0;
 
-void add_income() {
-    cin >> total_income; 
+
+void add_income(vector<Transaction> &transactions, float& total_income) {
+    Transaction new_transact; 
+    new_transact.type = "income"; 
+    cout << "What category is the transaction?";
+    cin >> new_transact.category;
+    cout << "What is the amount?";
+    cin >> new_transact.amount; 
+
+    transactions.push_back(new_transact);
+    total_income += new_transact.amount;
+
+
 }
 
-void add_expense() {
-    cin >> total_expenses; 
+void add_expense(vector<Transaction> &transactions, float&total_expenses) {
+    Transaction new_expense; 
+    new_expense.type = "expense";
+    cout << "What category is this expense?";
+    cin >> new_expense.category;
+    cout << "What is the amount?";
+    cin >> new_expense.amount;
+
+    transactions.push_back(new_expense);
+    total_expenses += new_expense.amount;
+
+
 }
 
 void show_balance() {
-    cout << "Balance: " << balance << endl;
+    
 }
 
 void show_spending_breakdown() {
-    cout << "Spending Breakdown: " << endl;
-    for (Transaction t : transactions) {
-        cout << t.category << ": " << t.amount << endl;
-    }
+    
 }
 
 int main() {
     //start with the structure 
+
+    vector<Transaction> transactions; 
+    float total_income = 0;
+    float total_expenses = 0;
+    float balance = 0;
+
     int input; 
     cout << "Menu: \n 1. Add Income \n 2. Add Expense \n 3. Show Balance \n 4. Show Spending Breakdown \n 5. Quit \n";
     cin >> input; 
     while (input != 5) {
         switch (input) {
             case 1: 
-                add_income();
+                add_income(transactions, total_income);
+                cout << total_income << endl;
                 break;
             case 2: 
-                add_expense();
+                add_expense(transactions, total_expenses);
+                cout << total_expenses << endl;
                 break; 
             case 3: 
                 show_balance();
+                cout << balance << endl;
                 break; 
             case 4: 
                 show_spending_breakdown();
+                break;
+            case 5: 
+                cout << "Goodbye!" << endl;
+                break;
+            default:
+                cout << "Invalid input" << endl;
+                break;
         }
     }
 
