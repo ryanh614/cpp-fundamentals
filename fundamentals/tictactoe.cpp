7929c3b1a9446a3229928937d619d4d1143c1f33
ryanh614 <ryanthausner@gmail.com>
2026-03-30 22:59:42 -0700

    print board function

--- fundamentals/tictactoe.cpp
+++ fundamentals/tictactoe.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <string>
-#include <cmath>
 using namespace std; 
 
+char board[3][3] = {{' ',' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
 
+
+
+void print_board(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        cout << " " << board[i][0] << " | " << board[i][1] << " | " << board[i][2] << endl;
+        if (i < 2) {
+            cout << "---|---|---" << endl; 
+        }
+}
+}
 
 int main() {
+    print_board(board);
     return 0; 
 }
