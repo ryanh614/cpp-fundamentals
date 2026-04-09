34e64f34f4e36bfb4516027ff521734bfeb4013f
ryanh614 <ryanthausner@gmail.com>
2026-04-08 17:06:57 -0700

    check win logic

--- fundamentals/tictactoe.cpp
+++ fundamentals/tictactoe.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 char board[3][3] = {{' ',' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
 
+//game logic: print board, x starts, check win, change counter, print board again, repeat
+
 
 
 void print_board(char board[3][3]) {
@@ -12,7 +14,25 @@ void print_board(char board[3][3]) {
         if (i < 2) {
             cout << "---|---|---" << endl; 
         }
+    }
 }
+
+bool check_win(char player) {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player) return true;
+        if (board[0][i] == player && board[1][i] == player && board[2][i] == player) return true;
+    }
+
+    if (board[0][0] == player && board[1][1] == player && board[2][2] == player) return true; 
+    if (board[0][2] == player && board[1][1] == player && board[2][0] == player) return true; 
+    return false; 
+}
+
+bool check_tie() {
+    for (int i = 0; i < 3; i++) 
+        for (int j = 0; j < 3; j++) 
+            if (board[i][j] == ' ') return false; 
+    return true; 
 }
 
 int main() {
